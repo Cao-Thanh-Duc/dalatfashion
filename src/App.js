7375ca49b08bde71feb0c30ae150f7e258e85dae
Cao-Thanh-Duc <caothanhduc.bdqb@gmail.com>
2024-11-25 23:28:20 +0700

    B1

---
+++ src/App.js
@@ -0,0 +1,75 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home";
+import Header from "./Components/Header";
+import { createContext, useEffect, useState } from "react";
+import axios from "axios";
+import Footer from "./Components/Footer";
+import ProducModal from "./Components/ProductModal";
+import Listing from "./Pages/Listing";
+import ProductDetails from "./Pages/ProductDetails";
+import Cart from "./Pages/Cart";
+import Introduction from "./Components/Introduction";
+import Contact from "./Components/Contact";
+const MyContext = createContext();
+
+function App() {
+  const [countryList, setCountryList] = useState([]);
+  const [selectedCountry, setselectedCountry] = useState("");
+  const [isOpenProductModal, setisOpenProductModal] = useState(false);
+
+  useEffect(() => {
+    getCountry("https://countriesnow.space/api/v0.1/countries/");
+  }, []);
+  const getCountry = async (url) => {
+    const responsive = await axios.get(url).then((res) => {
+      setCountryList(res.data.data);
+      console.log(res.data.data);
+    });
+  };
+
+  const values = {
+    countryList,
+    setselectedCountry,
+    selectedCountry,
+    isOpenProductModal,
+    setisOpenProductModal,
+  };
+
+  return (
+    <BrowserRouter>
+      <MyContext.Provider value={values}>
+        <Header />
+        <Routes>
+          <Route path="/" exact={true} element={<Home />} />
+          <Route path="/cat/:id" exact={true} element={<Listing />} />
+          <Route
+            path="/product/:id"
+            exact={true}
+            element={<ProductDetails />}
+          />
+          <Route path="/cart/:id" exact={true} element={<Cart />} />
+          <Route
+            path="/introduct-dalatfashion/:id"
+            exact={true}
+            element={<Introduction />}
+          />
+          <Route
+            path="/contact-dalatfashion/:id"
+            exact={true}
+            element={<Contact />}
+          />
+        </Routes>
+        <Footer />
+        {isOpenProductModal === true && <ProducModal closeProductModal />}
+      </MyContext.Provider>
+    </BrowserRouter>
+  );
+}
+
+export default App;
+
+export { MyContext };
+
+// Đã xem xong part 7
---
+++ src/Components/Header/Navigation/index.js
@@ -0,0 +1,293 @@
+import Button from "@mui/material/Button";
+import { useEffect, useState } from "react";
+import { FaAngleDown, FaAngleRight } from "react-icons/fa6";
+import { IoIosMenu } from "react-icons/io";
+import { Link, useLocation } from "react-router-dom";
+
+const Navigation = () => {
+  const [isopenSidebarVal, setisopenSidebarVal] = useState(false);
+  const location = useLocation();
+  const isActive = (path) => location.pathname === path;
+
+  useEffect(() => {
+    if (location.pathname === "/") {
+      setisopenSidebarVal(true);
+    } else {
+      setisopenSidebarVal(false);
+    }
+  }, [location]);
+
+  return (
+    <nav>
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-2 navPart1">
+            {location.pathname === "/" && (
+              <div className="catWrapper">
+                <Button
+                  className="allCartTab align-items-center"
+                  onClick={() => setisopenSidebarVal(!isopenSidebarVal)}
+                >
+                  <span className="icon1 mr-2">
+                    <IoIosMenu />
+                  </span>
+                  <span className="text"> DANH MỤC </span>
+                  <span className="icon2 ml-2">
+                    <FaAngleDown />
+                  </span>
+                </Button>
+                <div className={`sidebarnav ${isopenSidebarVal ? "open" : ""}`}>
+                  <ul>
+                    <li>
+                      <Link to="/">
+                        <Button>
+                          Nam <FaAngleRight className="ml-auto" />
+                        </Button>
+                      </Link>
+                      <div className="submenu">
+                        <Link to="/">
+                          <Button> Quần tây </Button>
+                        </Link>
+                        <Link to="/">
+                          <Button> Quần jean </Button>
+                        </Link>
+                        <Link to="/">
+                          <Button> Quần short </Button>
+                        </Link>
+                        <Link to="/">
+                          <Button> Quần kaki </Button>
+                        </Link>
+                        <Link to="/">
+                          <Button> Áo thun </Button>
+                        </Link>
+                        <Link to="/">
+                          <Button> Áo sơmi </Button>
+                        </Link>
+                        <Link to="/">
+                          <Button> Áo polo </Button>
+                        </Link>
+                        <Link to="/">
+                          <Button> Áo khoác </Button>
+                        </Link>
+                      </div>
+                    </li>
+                    <li>
+                      <Link to="/">
+                        <Button>
+                          Nữ <FaAngleRight className="ml-auto" />
+                        </Button>
+                      </Link>
+                      <div className="submenu">
+                        <Link to="/">
+                          <Button> Quần tây </Button>
+                        </Link>
+                        <Link to="/">
+                          <Button> Quần jean </Button>
+                        </Link>
+                        <Link to="/">
+                          <Button> Quần Legging </Button>
+                        </Link>
+                        <Link to="/">
+                          <Button> Chân váy </Button>
+                        </Link>
+                        <Link to="/">
+                          <Button> Váy Babydoll </Button>
+                        </Link>
+                        <Link to="/">
+                          <Button> Áo sơmi </Button>
+                        </Link>
+                        <Link to="/">
+                          <Button> Áo Croptop </Button>
+                        </Link>
+                        <Link to="/">
+                          <Button> Áo khoác </Button>
+                        </Link>
+                      </div>
+                    </li>
+                    <li>
+                      <Link to="/">
+                        <Button>
+                          Phụ kiện <FaAngleRight className="ml-auto" />
+                        </Button>
+                      </Link>
+                      <div className="submenu">
+                        <Link to="/">
+                          <Button> Giày dép</Button>
+                        </Link>
+                        <Link to="/">
+                          <Button> Túi xách </Button>
+                        </Link>
+                        <Link to="/">
+                          <Button> Mũ </Button>
+                        </Link>
+                        <Link to="/">
+                          <Button> Thắt lưng </Button>
+                        </Link>
+                      </div>
+                    </li>
+                    <li>
+                      <Link to="/">
+                        <Button>Tin Tức </Button>
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to="/introduct-dalatfashion/1">
+                        <Button>Giới thiệu </Button>
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to="/">
+                        <Button>Liên hệ </Button>
+                      </Link>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            )}
+          </div>
+
+          <div className="col-sm-10 navPart2 d-flex align-center">
+            <ul className="list list-inline w100">
+              <li className="list-inline-item">
+                <Link to="/cat/1">
+                  <Button> NAM </Button>
+                </Link>
+                <div className="submenu shadow">
+                  <Link to="/">
+                    <Button> Quần tây </Button>
+                  </Link>
+                  <Link to="/">
+                    <Button> Quần jean </Button>
+                  </Link>
+                  <Link to="/">
+                    <Button> Quần short </Button>
+                  </Link>
+                  <Link to="/">
+                    <Button> Quần kaki </Button>
+                  </Link>
+                  <Link to="/">
+                    <Button> Áo thun </Button>
+                  </Link>
+                  <Link to="/">
+                    <Button> Áo sơmi </Button>
+                  </Link>
+                  <Link to="/">
+                    <Button> Áo polo </Button>
+                  </Link>
+                  <Link to="/">
+                    <Button> Áo khoác </Button>
+                  </Link>
+                </div>
+              </li>
+              <li className="list-inline-item">
+                <Link to="/cat/1">
+                  <Button> UNISEX </Button>
+                </Link>
+                <div className="submenu shadow">
+                  <Link to="/">
+                    <Button> Quần tây </Button>
+                  </Link>
+                  <Link to="/">
+                    <Button> Quần jean </Button>
+                  </Link>
+                  <Link to="/">
+                    <Button> Quần short </Button>
+                  </Link>
+                  <Link to="/">
+                    <Button> Quần kaki </Button>
+                  </Link>
+                  <Link to="/">
+                    <Button> Áo thun </Button>
+                  </Link>
+                  <Link to="/">
+                    <Button> Áo sơmi </Button>
+                  </Link>
+                  <Link to="/">
+                    <Button> Áo polo </Button>
+                  </Link>
+                  <Link to="/">
+                    <Button> Áo khoác </Button>
+                  </Link>
+                </div>
+              </li>
+              <li className="list-inline-item">
+                <Link to="/cat/1">
+                  <Button> NỮ </Button>
+                </Link>
+                <div className="submenu shadow">
+                  <Link to="/">
+                    <Button> Quần tây </Button>
+                  </Link>
+                  <Link to="/">
+                    <Button> Quần jean </Button>
+                  </Link>
+                  <Link to="/">
+                    <Button> Quần Legging </Button>
+                  </Link>
+                  <Link to="/">
+                    <Button> Chân váy </Button>
+                  </Link>
+                  <Link to="/">
+                    <Button> Váy Babydoll </Button>
+                  </Link>
+                  <Link to="/">
+                    <Button> Áo sơmi </Button>
+                  </Link>
+                  <Link to="/">
+                    <Button> Áo Croptop </Button>
+                  </Link>
+                  <Link to="/">
+                    <Button> Áo khoác </Button>
+                  </Link>
+                </div>
+              </li>
+              <li className="list-inline-item">
+                <Link to="/cat/1">
+                  <Button> PHỤ KIỆN </Button>
+                </Link>
+                <div className="submenu shadow">
+                  <Link to="/">
+                    <Button> Giày dép</Button>
+                  </Link>
+                  <Link to="/">
+                    <Button> Túi xách </Button>
+                  </Link>
+                  <Link to="/">
+                    <Button> Mũ </Button>
+                  </Link>
+                  <Link to="/">
+                    <Button> Thắt lưng </Button>
+                  </Link>
+                </div>
+              </li>
+              <li className="list-inline-item">
+                <Link to="/">
+                  <Button> TIN TỨC </Button>
+                </Link>
+              </li>
+              <li className="list-inline-item ">
+                <Link to="/introduct-dalatfashion/1">
+                  <Button
+                    className={
+                      isActive("/introduct-dalatfashion/1") ? "activeGT" : ""
+                    }
+                  >
+                    {" "}
+                    GIỚI THIỆU{" "}
+                  </Button>
+                </Link>
+              </li>
+              <li className="list-inline-item">
+                <Link to="contact-dalatfashion/1">
+                  <Button> LIÊN HỆ </Button>
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navigation;
